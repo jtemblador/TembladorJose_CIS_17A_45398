7510b19232245e190833da6a7e7abfe72c6bdb75
Jose Temblador <tembladorjose@yahoo.com>
2021-06-24 19:05:25 -0700

    introlab 6/24/21

---
+++ HW/CPPTemplate_1/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Jose Temblador
+ * Created on January 4, 2021, 10:50 AM
+ * Purpose:  CPP Template 
+ *           To be copied for each Assignment Problem
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Math, Science, Universal, Conversions, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Seed the random number function
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Process/Map inputs to outputs
+    
+    //Output data
+    
+    //Exit stage right!
+    return 0;
+}
---
+++ HW/Hello_World/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   Hello_World.cpp
+ * Author: Jose Temblador
+ * Created on June 24, 2021, 6:58 PM
+ * Purpose:  Hello World program
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Math, Science, Universal, Conversions, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Seed the random number function
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Process/Map inputs to outputs
+    
+    //Output data
+    cout<<"Hello World!";
+    //Exit stage right!
+    return 0;
+}
